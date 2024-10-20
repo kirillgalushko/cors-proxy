802a09710aae40c40a4e8db5b40b3e424c0d3729
Kirill Galushko <yoursgk@mail.ru>
2024-10-20 17:11:17 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3122;
+
+app.use(cors());
+
+app.get('/proxy', async (req, res) => {
+    const targetUrl = req.query.url;
+
+    if (!targetUrl) {
+        return res.status(400).send('URL is required');
+    }
+
+    try {
+        const response = await axios.get(targetUrl);
+        res.send(response.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error fetching the URL');
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Proxy server is running on http://localhost:${PORT}`);
+});
